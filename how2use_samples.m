fc1f46b28eca0113c60742c3244744e8f6479a4b
jbecherer <jbecherer@gmx.net>
2017-11-21 13:59:49 -0800

    introduced sample script to test the other routines

---
+++ how2use_samples.m
@@ -0,0 +1,13 @@
+%% this files shows examples on how to use the different routines of this package
+
+addpath(genpath('./'));
+
+
+%_____________________database selects______________________
+experiment = 'is17';
+platform   = 'ms100%';
+instType   = 'gust%';
+inst_sn    = '%';
+
+%_____________________add datapaths to database______________________
+% add_instrument_pathes2database(path2ganges, [experiment], [platform], [instType], [inst_sn])
