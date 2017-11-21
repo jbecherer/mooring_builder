4e8c4c12cf7a9ae4a175a85d908437b8f1dfa378
jbecherer <jbecherer@gmx.net>
2017-11-21 13:59:11 -0800

    made routine ready for general usage

--- database/add_instrument_pathes2database.m
+++ database/add_instrument_pathes2database.m
@@ -1,8 +1,46 @@
+function [] = add_instrument_pathes2database(path2ganges, experiment, platform, instType, inst_sn )
+%%     [] = add_instrument_pathes2database(path2ganges, [experiment], [platform], [instType], [inst_sn])
+%
+%  This functions automatically tries to find datapathes and adds them to the omg database
+%
+%  INPUT
+%     path2ganges    :  local path to ganges on computer (default '/data/' corresonding to matlab server)
+%     experiment     :  string to identify the experiemnt (default '%' which means all experiments) 
+%     platform       :  string to identify the platforms (default '%' which means all platforms) 
+%     instType       :  string to identify the type of instrument (default '%' which means all types) 
+%     inst_sn        :  string to identify the serial number of instrument (default '%' which means all sns) 
+%
+%
+%   created by: 
+%        Johannes Becherer
+%        Tue Nov 21 13:18:21 PST 2017
 
-%path2ganges = '/home/johannes/'
-path2ganges = '/Users/mixing/'
+%_____________________set default inputs______________________
+if nargin < 5
+   inst_sn = '%';
+   disp( '---select all serial numbers---');
+end
 
-experiment = 'is17';
+if nargin < 4
+   instType = '%';
+   disp( '---select all instruments types---');
+end
+
+if nargin < 3
+   platform = '%';
+   disp( '---select all platforms---');
+end
+
+if nargin < 2
+   experiment = '%';
+   disp( '---select all experiments---');
+end
+
+if nargin < 1
+   path2ganges = '/data/';
+   disp( '!!!Where is ganges on your computer???');
+end
+
 
 
 %_____________________Ask the data base______________________
@@ -10,10 +48,22 @@ experiment = 'is17';
 omg_db = sqlite([path2ganges 'ganges/work/database/omg.sqlite']);
 
 % find all instruments in database that belong to mooring
-data = fetch(omg_db, ['SELECT inst_name, inst_id,  datapath, inst_type ' ...
-                     'FROM instOnPlat where experiment like "' experiment '"']);
+sql_string = ['SELECT inst_name, inst_id,  datapath, inst_type ' ...
+                     'FROM instOnPlat where experiment like "' experiment '" and ' ...
+                     'platform_name like "' platform  '" and inst_type like "' instType '" and '...
+                     'inst_name like "' inst_sn '"'];
+   disp('The following SQL statement is applied to the omg_database:');
+   disp(['  ' sql_string]);
+data = fetch(omg_db, sql_string );
+
+%_____________________show all instruments that are going to be ______________________
+disp('all these instruments are going to be handled:');
+disp(char(data{:,1}));
+
+
+
       
-% loop through all instruments
+%_______________loop through all instruments_______________________
 for i = 1:size(data,1)
    inst_name = char(data{i,1});
    inst_id   = data{i,2};
@@ -56,5 +106,5 @@ for i = 1:size(data,1)
 
 end
 
-
+% close database
 close(omg_db);
