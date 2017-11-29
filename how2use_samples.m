156161250906c1f2df5ed15a4fde20f904981e49
jbecherer <jbecherer@gmx.net>
2017-11-29 15:57:51 -0800

    added example for Praw processing

--- how2use_samples.m
+++ how2use_samples.m
@@ -1,6 +1,7 @@
 %% this files shows examples on how to use the different routines of this package
 
-path2ganges = '/home/johannes/';
+%path2ganges = '/home/johannes/';
+path2ganges = '/Users/mixing/';
 
 addpath(genpath('./sw_tbx/'));
 addpath(genpath('./database/'));
@@ -27,7 +28,7 @@ inst_sn    = '%';
 %_____________________add datapaths to database______________________
 %  platform = '%';
 %  insttype = '%';
-%add_instrument_pathes2database(path2ganges, [experiment], [platform], [instType], [inst_sn])
+%  add_instrument_pathes2database(path2ganges, [experiment], [platform], [instType], [inst_sn])
 
 %_____________________add _comments to instruments in database______________________
 %  inst_sn = 'G064';
@@ -42,17 +43,25 @@ inst_sn    = '%';
   %add_auto_startstop2instruments(path2ganges, [experiment], [platform], [instType], [inst_sn])
 
 %_____________________temp.mat processing______________________
-%chipod_or_gust = 0;
-%do_redo = 0;
-%inst_sn = '1123'
-%generate_all_tempmat(path2ganges, experiment, platform, chipod_or_gust , inst_sn, do_redo)
+   %chipod_or_gust = 0;
+   %do_redo = 0;
+   %inst_sn = '1123'
+   %generate_all_tempmat(path2ganges, experiment, platform, chipod_or_gust , inst_sn, do_redo)
 
 %_____________________make bsic temp_plots______________________
-%inst_sn = 'G041';
-%plot_temp_for_gust_and_chipods(path2ganges, experiment, platform, instType, inst_sn )
+   %inst_sn = 'G041';
+   %plot_temp_for_gust_and_chipods(path2ganges, experiment, platform, instType, inst_sn )
+
+%_____________________process Praw______________________
+%  chipod_or_gust = 0;
+%  do_redo        = 1;
+%  do_parallel    = 1;
+%  inst_sn        = 'G044'
+%  generate_all_praw(path2ganges, experiment, platform, chipod_or_gust, inst_sn, do_redo, do_parallel );
+
 
 %_____________________calibrate all pitot tubes______________________
-chipod_or_gust = 0;
-do_redo        = 0;
-do_parallel    = 1;
-calibrate_all_pitots(path2ganges, experiment, platform, chipod_or_gust, inst_sn, do_redo )
+%  chipod_or_gust = 0;
+%  do_redo        = 0;
+%  do_parallel    = 1;
+%  calibrate_all_pitots(path2ganges, experiment, platform, chipod_or_gust, inst_sn, do_redo )
